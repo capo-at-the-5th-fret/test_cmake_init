7a0e4d8f77be99cc8264585fb01a07b818690e2f
capo-at-the-5th-fret <apostrophe1974@gmail.com>
2023-05-05 23:16:18 -0400

    Added workflow_dispatch to workflow

--- test/source/test_cmake_init_test.cpp
+++ test/source/test_cmake_init_test.cpp
@@ -1,8 +1,17 @@
 #include "test_cmake_init/test_cmake_init.hpp"
 
+
+
+void foo(int x)
+{
+    int buf[10];
+    buf[x] = 0; // <- ERROR
+    if (x == 1000) {}
+}
+
 auto main() -> int
 {
   auto const result = name();
-
+  foo(0);
   return result == "test_cmake_init" ? 0 : 1;
 }
